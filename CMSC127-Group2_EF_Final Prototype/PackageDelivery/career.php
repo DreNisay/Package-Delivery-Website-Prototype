17feea700e396be3c4fc7a4a92804655cadafabe
Dre Nisay <104890890+DreNisay@users.noreply.github.com>
2022-06-27 18:33:56 +0800

    Uploaded Files

---
+++ CMSC127-Group2_EF_Final Prototype/PackageDelivery/career.php
@@ -0,0 +1,101 @@
+<html>
+<head>
+<?php 
+
+  include_once 'header.php';
+
+  
+  ?>
+  <meta charset="utf-8">
+
+<link rel="stylesheet" type="text/css" href="css/career.css" />
+<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+  
+
+
+<div class="joinlogo">
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<center>  <div class="join">Join The Team</div>
+<br>
+<div class="words">We are looking forwand in joining us in this journey.</words>
+<br>
+A tough but fulfiling one!
+<br>
+<br>
+<br>
+<br>
+Send a resume and email at contact_ph@bluefalcon.co
+<br>
+<br>
+We will contact on your application
+<br>
+</center>
+</div>
+
+<div class="pic">
+<img class="team" src="team.png" width="700px" height="600px">
+</div>
+<br>
+<div class="empty">
+</div>
+<div class="manual">
+<p style="color:#1E90FF">Steps for Application:</p>
+<ol class="step">
+	<li>Send a resume through email: contact_ph@bluefalcon.co </li>	
+	<li>Wait for your the application status.</li>
+	<li>Once approved,we will send an email about an online interview</li>
+	<li>Online Interview</li>
+	<li>Wait for the status of your application</li>
+</ol>
+</div>
+<div class="empty">
+</div>
+<div class="claim">
+<br>
+<hr class="mid">
+<br>	
+We are looking forward meeting you!
+<br>
+<br>
+<br>
+<div class="box">
+	<img class="employ" src="employ.png" width="250px" height="250px">
+<br>
+<b><p style="color:#1E90FF;font-size:160%">Application is still open </p></b>
+<br>
+We are welcoming any good individuals for our company
+<br>
+</div>
+<br>
+<br>
+<hr class="mid">
+</div>
+<br>
+<br>
+
+<div class="family">
+<br>
+<br>
+<br>
+	Meet new people and be part of the family
+	<img class="employees" src="employees.png" width="800px" height="500px">
+	<br>
+<br>
+</div>
+
+<?php
+
+    
+?>
+</body>
+</html>
---
+++ CMSC127-Group2_EF_Final Prototype/PackageDelivery/homepage.php
@@ -0,0 +1,165 @@
+<html>
+<head>
+<?php 
+
+  include_once 'header.php';
+
+  
+  ?>
+  <meta charset="utf-8">
+
+<link rel="stylesheet" type="text/css" href="homepage.css" />
+<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+	
+
+
+<div class="clearfix">
+
+	<div class= "motto">
+
+		<h1 style="font-size:240%">Your Package is in our Safe Hands</h1>
+	<br>
+	<p style="color:black;font-size:150%"><b>" We deliver your parcel in a safe and quick way</b></p>
+	
+	<p style="color:black;font-size:150%"><b>Because we care about the gifts you give to others "</b></p>
+	</div>	
+	
+	<div class= "photo">
+		<img src="delivery man.jpg" width= "600" height="400" class="dman">
+	</div>
+
+</div>
+<br>
+<br>
+
+<div class="quality">
+<br>
+<br>
+<br>
+<center><h1 class="promotion0">Enjoy stress-free deliveries with Blue Falcon Philippines</h1></center>
+<br>
+<br>
+<br>
+<div class="list">
+
+<div class="first">
+	<div class="char"><img src="truck.png" class="char"></div>
+	<br>
+	<br>
+	<h2>Workable parcel pickups</h2>
+	<br>
+	Choose from different pick-up/drop-off options 
+	and simply make reservations via our dashboard.
+</div>	
+
+<div class="second">
+	<div class="char"><img src="cash.png" class="char"></div>
+	<br>
+	<br>
+	<h2>Reliable Cash-On-Delivery</h2>
+	<br>
+	We offer cash-on-delivery services that are guranteed good deals
+	<br>
+	in all areas that we cover.
+</div>
+
+<div class="third">
+	<div class="char"><img src="staff.png" class="char"></div>
+	<br>
+	<br>
+	<h2>Attentive Customer Service</h2>
+	<br>
+	Our staff are always attentive to any your problems or help occur
+	<br>
+	in all areas that we cover.
+</div>
+
+<div class="fourth"> 
+	<div class="char"><img src="order.png" class="char"></div>
+	<br>
+	<br>
+	<h2>Simple Order Creation</h2>
+	<br>
+	Comfortably create new shipments and manage<br>
+	existing ones via a personalised dashboard.
+</div>
+
+</div>
+
+</div>
+
+<div class="empty">
+</div>
+
+
+<div class="steps">
+<br>
+<br>
+<br>
+<br>
+<center><h1 class="promotion1">Let's start shipping</h1></center>
+<center>
+		<ul class="quickie"> 
+		<li class="step1"><h2>01</h2>
+		<br>
+		<br> 
+			<img src="account.png" width="200" height="200">
+		<br>
+		<h2>Create an account</h2>
+		<br>
+		<br>
+		Get started in less than 5 minutes 
+		<br>
+		</li>
+		<li class="step2"><h2>02</h2>
+		<br>
+		<br> 
+			<img src="parcel.png" width="200" height="200">
+		<br>
+		<h2>Send Parcels</h2>
+		<br>
+		<br>
+		Book or upload of orders in bulk via dashboard
+		<br>
+		</li>
+		<li class="step3"><h2>03</h2>
+		<br>
+		<br> 
+			<img src="points.png" width="200" height="200">
+		<br>
+		<h2>Earn points & enjoy priviliges</h2>
+		<br>
+		<br>
+		Redeem points in exchange for gadgets & more 
+		<br>
+		</li>
+		</ul>
+</center>
+<br>
+<br>
+<br>
+<br>
+</div>	
+
+
+<div class="contact">
+<div class="contactnumber">Contact Us</div>
+<br>
+<br>
+Phone number:    09123456789
+<br>
+Email:     contact_ph@bluefalcon.co
+</div>
+<div class="empty">
+</div>
+
+
+<?php
+
+    
+?>
+</body>
+</html>
+
